2d70aae0f380be6d98e95f9dd5c4a6155d21fbb0
Tantely Andrianarivola <tantely.and@onja.org>
2020-07-18 08:20:18 +0300

    first commit with the plan in it

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('good luck!');
+//To do list:
+//Add and show the modal: grab the button and the inner and the aouter element
+//create a function to show the modal
+//add an event listener to the buttons
+//add and event listener to the window and the escape key and its function
+//add the the form to the inner html when it gets shown in the modal
+//add the click button to submit the order
+//show the chosen list to the screen
+//delete button to delete the entry
